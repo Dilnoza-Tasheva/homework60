aa1ba7557f2f3d2c0952d37f81a8d9f6b2add647
Dilnoza Tasheva <dilnozatasheva@gmail.com>
2024-10-14 09:27:19 +0200

    Created two main components and populated them with as per the requirements

---
+++ src/Components/MessageItem/MessageItem.tsx
@@ -0,0 +1,12 @@
+
+
+const MessageItem = () => {
+  return (
+    <div>
+      <input type="text" placeholder="your message"/>
+      <button className="btn btn-primary">Send</button>
+    </div>
+  );
+};
+
+export default MessageItem;
---
+++ src/Components/Messages/Messages.tsx
@@ -0,0 +1,13 @@
+
+
+const Messages = () => {
+  return (
+    <div>
+      <ul>
+        <li><strong>Author and message</strong></li>
+      </ul>
+    </div>
+  );
+};
+
+export default Messages;
